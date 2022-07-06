76c8dc4d2b494c7ce42d3d850830880d4079cfa3
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2022-07-05 23:02:09 -0300

    Gerenciando dependências com composer

--- index.php
+++ index.php
@@ -2,36 +2,7 @@
 
 use Tarefas\Models\RepositorioTarefas;
 
-spl_autoload_register(function ($classe) {
-    $partes = explode('\\', $classe);
-
-    if (count($partes) == 1) {
-        if (is_file(__DIR__."/models/{$classe}.php")) {
-            require_once __DIR__."/models/{$classe}.php";
-            return;
-        }
-    }
-
-    if ($partes[0] !== 'Tarefas') {
-        return;
-    }
-
-    array_shift($partes);
-    $classe = array_pop($partes);
-
-    $caminho = [];
-    foreach ($partes as $pasta) {
-        $caminho[] = strtolower($pasta);
-    }
-
-    $caminho[] = $classe.'.php';
-    $arquivo = __DIR__.'/'.implode('/', $caminho);
-
-    if (is_file($arquivo)) {
-        require_once $arquivo;
-    }
-});
-
+require "vendor/autoload.php";
 require "config.php";
 require "helpers/banco.php";
 require "helpers/ajudantes.php";
